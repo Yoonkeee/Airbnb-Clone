c23390df7fb5b67776f791131b1c7178b5264fc2
yoonki <kimyk@harami.co.kr>
2022-10-25 16:49:50 +0900

    Ch.11 - #16 Upload Photos

--- airbnb-clone-backend/rooms/views.py
+++ airbnb-clone-backend/rooms/views.py
@@ -14,6 +14,7 @@ from rest_framework.exceptions import (
 )
 from reviews.serializers import ReviewSerializer
 from django.conf import settings
+from medias.serializers import PhotoSerializer
 
 
 class Amenities(APIView):
@@ -175,5 +176,22 @@ class RoomReviews(APIView):
 
 
 class RoomPhotos(APIView):
-    def post(self, request):
-        pass
+    def get_object(self, pk):
+        try:
+            return Room.objects.get(pk=pk)
+        except Room.DoesNotExist:
+            raise NotFound
+
+    def post(self, request, pk):
+        room = self.get_object(pk)
+        if not request.user.is_authtenticated:
+            raise NotAuthenticated
+        if request.user != room.owner:
+            raise PermissionDenied
+        serializer = PhotoSerializer(data=request.data)
+        if serializer.is_valid():
+            photo = serializer.save(room=room)
+            serializer = PhotoSerializer(photo)
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
