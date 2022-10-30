f09e1e3dd80ad0e85fa504c2ee893193b544adf3
yoonki <kimyk@harami.co.kr>
2022-10-30 22:28:16 +0900

    Ch.11 - #23 Create a Booking

--- airbnb-clone-backend/bookings/serializers.py
+++ airbnb-clone-backend/bookings/serializers.py
@@ -1,5 +1,34 @@
 from rest_framework import serializers
 from .models import Booking
+from django.utils import timezone
+
+
+class CreateRoomBookingSerializer(serializers.ModelSerializer):
+
+    check_in = serializers.DateField()
+    check_out = serializers.DateField()
+
+    class Meta:
+        model = Booking
+        fields = (
+            "check_in",
+            "check_out",
+            "guests",
+        )
+
+    def validate_check_in(self, value):
+        now = timezone.localtime(timezone.now()).date()
+        if now > value:
+            raise serializers.ValidationError("날짜가 이상함")
+        # value = validate_field
+        return value
+
+    def validate_check_out(self, value):
+        now = timezone.localtime(timezone.now()).date()
+        if now > value:
+            raise serializers.ValidationError("날짜가 이상함")
+        # value = validate_field
+        return value
 
 
 class PublicBookingSerializer(serializers.ModelSerializer):
