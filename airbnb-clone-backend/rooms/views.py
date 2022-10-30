506a5c20453cc038dbd2865efe09ef479d856dee
yoonki <kimyk@harami.co.kr>
2022-10-30 23:14:04 +0900

    Ch.11 - #25 Booking Completed

--- airbnb-clone-backend/rooms/views.py
+++ airbnb-clone-backend/rooms/views.py
@@ -240,6 +240,12 @@ class RoomBookings(APIView):
         room = self.get_object(pk)
         serializer = CreateRoomBookingSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
+            booking = serializer.save(
+                room=room,
+                user=request.user,
+                kind=Booking.BookingKindChoices.ROOM,
+            )
+            serializer = PublicBookingSerializer(booking)
+            return Response(serializer.data)
         else:
             return Response(serializer.errors)
