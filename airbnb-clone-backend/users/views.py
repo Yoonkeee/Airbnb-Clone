4c1bdbbbc48711820bef56026423693eb0fc7e8d
yoonki <kimyk@harami.co.kr>
2022-11-01 00:16:44 +0900

    Ch.12 - #02 [USERS API] - Change Password

--- airbnb-clone-backend/users/views.py
+++ airbnb-clone-backend/users/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
-from . import serializers
+from . import serializers, models
 from rest_framework import exceptions
 
 
@@ -44,3 +44,30 @@ class Users(APIView):
             return Response(serializer.data)
         else:
             return Response(serializer.errors)
+
+
+class PublicUser(APIView):
+    def get(self, request, username):
+        try:
+            user = models.User.objects.get(username=username)
+        except models.User.DoesNotExist:
+            raise exceptions.NotFound
+        serializer = serializers.PrivateUserSerializer()
+        return Response
+
+
+class ChangePassword(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def put(self, request):
+        user = request.user
+        old_password = request.data.get("old_password")
+        new_password = request.data_get("new_password")
+        if not old_password or not new_password:
+            raise exceptions.ParseError
+        if user.check_password(old_password):
+            user.set_password(new_password)
+            user.save()
+            return Response(status=status.HTTP_200_OK)
+        else:
+            raise exceptions.ParseError
