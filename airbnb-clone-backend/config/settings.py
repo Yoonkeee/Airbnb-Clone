2f02bb75117f42b8cea78125368f1f1edb2faf4e
yoonki <kimyk@harami.co.kr>
2022-11-13 00:49:40 +0900

    Ch.15 - #06 [AUTHENTICATION] - Environment Files

--- airbnb-clone-backend/config/settings.py
+++ airbnb-clone-backend/config/settings.py
@@ -11,16 +11,20 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import environ
+
+env = environ.Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-r78$*n46p28#6d+((*n$=w-!7h=lc-5gedrntz885)!q(y_qkd"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
