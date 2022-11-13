20d591780bd879670757401fc7bb11ee44e91d44
yoonki <kimyk@harami.co.kr>
2022-11-13 16:43:25 +0900

    Ch.16 - #01 [API TESTING] - Our First Test

--- airbnb-clone-backend/rooms/tests.py
+++ airbnb-clone-backend/rooms/tests.py
@@ -1,3 +1,7 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class TestAmenities(APITestCase):
+    def test_t(self):
+
+        self.assertEqual(2 + 2, 5, "ERROR MESSAGE~~")
