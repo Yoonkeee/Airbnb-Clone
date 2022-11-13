5d44c331ab604be8f21cde4a6aeba7523c1c96fc
yoonki <kimyk@harami.co.kr>
2022-11-13 17:09:13 +0900

    Ch.16 - #03 [API TESTING] - Create Amenity Test

--- airbnb-clone-backend/rooms/tests.py
+++ airbnb-clone-backend/rooms/tests.py
@@ -6,6 +6,7 @@ class TestAmenities(APITestCase):
 
     NAME = "Amenity Test"
     DSECRIPTION = "Amenity Description"
+    URL = "/api/rooms/amenities/"
 
     def setUp(self):
         models.Amenity.objects.create(
@@ -15,7 +16,7 @@ class TestAmenities(APITestCase):
 
     def test_all_amenities(self):
 
-        response = self.client.get("/api/rooms/amenities/")
+        response = self.client.get(self.URL)
         data = response.json()
         self.assertEqual(response.status_code, 200, "erorror")
 
@@ -23,3 +24,23 @@ class TestAmenities(APITestCase):
         self.assertEqual(len(data), 1)
         self.assertEqual(data[0]["name"], self.NAME)
         self.assertEqual(data[0]["description"], self.DSECRIPTION)
+
+    def test_create_amenity(self):
+        new_amenity_name = "New Amenity"
+        new_amenity_description = "New Desc"
+        response = self.client.post(
+            self.URL,
+            data={
+                "name": new_amenity_name,
+                "description": new_amenity_description,
+            },
+        )
+        data = response.json()
+        self.assertEqual(response.status_code, 200, "error!!")
+        self.assertEqual(data["name"], new_amenity_name)
+        self.assertEqual(data["description"], new_amenity_description)
+
+        response = self.client.post(self.URL)
+        data = response.json()
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("name", data)
