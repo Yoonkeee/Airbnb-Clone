d0f94f1e5d64dd9a10729273d7fa57aced49cf23
yoonki <kimyk@harami.co.kr>
2022-11-13 16:52:42 +0900

    Ch.16 - #02 [API TESTING] - Amenities Test

--- airbnb-clone-backend/rooms/tests.py
+++ airbnb-clone-backend/rooms/tests.py
@@ -1,7 +1,25 @@
 from rest_framework.test import APITestCase
+from . import models
 
 
 class TestAmenities(APITestCase):
-    def test_t(self):
 
-        self.assertEqual(2 + 2, 5, "ERROR MESSAGE~~")
+    NAME = "Amenity Test"
+    DSECRIPTION = "Amenity Description"
+
+    def setUp(self):
+        models.Amenity.objects.create(
+            name=self.NAME,
+            description=self.DSECRIPTION,
+        )
+
+    def test_all_amenities(self):
+
+        response = self.client.get("/api/rooms/amenities/")
+        data = response.json()
+        self.assertEqual(response.status_code, 200, "erorror")
+
+        self.assertIsInstance(data, list)
+        self.assertEqual(len(data), 1)
+        self.assertEqual(data[0]["name"], self.NAME)
+        self.assertEqual(data[0]["description"], self.DSECRIPTION)
