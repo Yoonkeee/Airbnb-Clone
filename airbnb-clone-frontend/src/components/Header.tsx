ee32204cf2ffdc6e286a784c7f6bed4345e72c00
yoonki <kimyk@harami.co.kr>
2022-11-17 23:42:14 +0900

    Ch.18 - #03 [CHAKRA UI] Dark Mode

--- airbnb-clone-frontend/src/components/Header.tsx
+++ airbnb-clone-frontend/src/components/Header.tsx
@@ -1,23 +1,43 @@
-import { Box, Button, HStack, IconButton, useDisclosure } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  HStack,
+  IconButton,
+  LightMode,
+  useColorMode,
+  useColorModeValue,
+  useDisclosure,
+} from "@chakra-ui/react";
 import { SiTesla } from "react-icons/si";
-import { FaMoon } from "react-icons/fa";
+import { FaMoon, FaSun } from "react-icons/fa";
 import LoginModal from "./LoginModal";
 import SignUpModal from "./SignUpModal";
 
 export default function Header() {
   const { isOpen: isLoginOpen, onClose: onLoginClose, onOpen: onLoginOpen } = useDisclosure();
   const { isOpen: isSignUpOpen, onClose: onSignUpClose, onOpen: onSignUpOpen } = useDisclosure();
+  const { colorMode, toggleColorMode } = useColorMode();
+  const logoColor = useColorModeValue("red.500", "red.200");
+  const Icon = useColorModeValue(FaMoon, FaSun);
   return (
     <HStack justifyContent={"space-between"} px={"10"} py={"5"} borderBottomWidth={1}>
-      <Box color="red.500">
+      <Box color={logoColor}>
         <SiTesla size={"48"} />
       </Box>
       <HStack spacing={2}>
-        <IconButton variant={"ghost"} aria-label={"다크모드"} icon={<FaMoon />} />
+        <IconButton
+          onClick={toggleColorMode}
+          variant={"ghost"}
+          aria-label={"다크모드"}
+          icon={<Icon />}
+        />
+
         <Button onClick={onLoginOpen}>로긴</Button>
-        <Button onClick={onSignUpOpen} colorScheme={"red"}>
-          횐가입
-        </Button>
+        <LightMode>
+          <Button onClick={onSignUpOpen} colorScheme={"red"}>
+            횐가입
+          </Button>
+        </LightMode>
       </HStack>
       <LoginModal isOpen={isLoginOpen} onClose={onLoginClose} />
       <SignUpModal isOpen={isSignUpOpen} onClose={onSignUpClose} />
