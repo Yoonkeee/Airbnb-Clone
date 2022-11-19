181b08fca384c41c73dc40539a13b0b0bff563cb
yoonki <yoonkeee@gmail.com>
2022-11-19 15:46:21 +0900

    Ch.19 - #02 [REACT QUERY] React Query

--- airbnb-clone-frontend/src/routes/Home.tsx
+++ airbnb-clone-frontend/src/routes/Home.tsx
@@ -52,7 +52,8 @@ export default function Home() {
       ) : null}
       {data?.map((room) => (
         <Room
-          imageUrl="https://a0.muscache.com/im/pictures/miso/Hosting-717134404264905813/original/dfe9fd1e-a010-43c9-b546-0bbc7d59f7f3.jpeg?im_w=720"
+          imageUrl={room.photos[0].file}
+          // imageUrl="https://a0.muscache.com/im/pictures/miso/Hosting-717134404264905813/original/dfe9fd1e-a010-43c9-b546-0bbc7d59f7f3.jpeg?im_w=720"
           name={room.name}
           rating={room.rating}
           city={room.city}
