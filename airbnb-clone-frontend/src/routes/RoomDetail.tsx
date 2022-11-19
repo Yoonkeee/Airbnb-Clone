17431a8fcfda7e8ae865b8237e49a41239727c1b
yoonki <yoonkeee@gmail.com>
2022-11-20 01:10:42 +0900

    Ch.19 - #07 [REACT QUERY] Reviews

--- airbnb-clone-frontend/src/routes/RoomDetail.tsx
+++ airbnb-clone-frontend/src/routes/RoomDetail.tsx
@@ -1,13 +1,28 @@
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
-import { getRoom } from "../api";
-import { IRoomDetail } from "../types";
-import { Box, Grid, GridItem, Heading, Image, Skeleton } from "@chakra-ui/react";
+import { getRoom, getRoomReviews } from "../api";
+import { IReview, IRoomDetail } from "../types";
+import {
+  Avatar,
+  Box,
+  Grid,
+  GridItem,
+  Heading,
+  HStack,
+  Image,
+  Skeleton,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import { FaStar } from "react-icons/fa";
 
 export default function RoomDetail() {
   const { roomPk } = useParams();
   const { isLoading, data } = useQuery<IRoomDetail>(["rooms", roomPk], getRoom);
-  console.log(data);
+  const { isLoading: isReviewsLoading, data: reviewsData } = useQuery<IReview[]>(
+    ["rooms", roomPk, "reviews"],
+    getRoomReviews
+  );
   return (
     <Box pb={40} mt={10} px={{ base: 10, lg: 40 }}>
       <Skeleton h={"43px"} w={"25%"} isLoaded={!isLoading}>
@@ -35,6 +50,28 @@ export default function RoomDetail() {
           </GridItem>
         ))}
       </Grid>
+      <HStack w={"40%"} justifyContent={"space-between"} mt={10}>
+        <VStack alignItems={"flex-start"}>
+          <Skeleton isLoaded={!isLoading} height={"30%"}>
+            <Heading fontSize={"2xl"}>이 집은 {data?.owner.name} 님 꺼임</Heading>
+            <HStack justifyContent={"flex-start"} w={"100%"}>
+              <Text>{data?.toilets} 화장실</Text>
+              <Text>•</Text>
+              <Text>{data?.rooms} 방</Text>
+            </HStack>
+          </Skeleton>
+        </VStack>
+        <Avatar name={data?.owner.name} size={"xl"} src={"data?.owner.avatar"} />
+      </HStack>
+      <Box mt={10}>
+        <Heading fontSize={"2xl"}>
+          <HStack>
+            <FaStar /> <Text>{data?.rating}</Text>
+            <Text>•</Text>
+            <Text>리뷰 {reviewsData?.length}개</Text>
+          </HStack>
+        </Heading>
+      </Box>
     </Box>
   );
 }
