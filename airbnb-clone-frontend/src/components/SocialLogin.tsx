e2eb7f2f1b8862430ffb1e27076e40acdd2deea7
yoonki <yoonkeee@gmail.com>
2022-11-21 22:44:04 +0900

    Ch.20 - #05 [AUTHENTICATION] Github Login

--- airbnb-clone-frontend/src/components/SocialLogin.tsx
+++ airbnb-clone-frontend/src/components/SocialLogin.tsx
@@ -6,18 +6,22 @@ export default function SocialLogin() {
     <Box>
       <HStack my={5}>
         <Divider />
-        <Text
-          textTransform={"uppercase"}
-          color={"gray.400"}
-          fontSize={"xs"}
-          as={"b"}
-        >
+        <Text textTransform={"uppercase"} color={"gray.400"} fontSize={"xs"} as={"b"}>
           Or
         </Text>
         <Divider />
       </HStack>
       <HStack>
-        <Button w={"50%"} leftIcon={<FaGithub />} colorScheme={"telegram"}>
+        <Button
+          as={"a"}
+          href={
+            "https://github.com/login/oauth/authorize?" +
+            "client_id=a422a39e972841a14bec&scope=read:user,user:email"
+          }
+          w={"50%"}
+          leftIcon={<FaGithub />}
+          colorScheme={"telegram"}
+        >
           깃헙으로 로그인
         </Button>
         <Button w={"50%"} leftIcon={<FaComment />} colorScheme={"yellow"}>
