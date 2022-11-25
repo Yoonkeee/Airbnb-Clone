f86d37dcb8bebe0e2b828172020449525ff5be30
yoonki <yoonkeee@gmail.com>
2022-11-26 00:07:39 +0900

    Ch.20 - #10 [AUTHENTICATION] Kakao Talk Auth

--- airbnb-clone-frontend/src/router.tsx
+++ airbnb-clone-frontend/src/router.tsx
@@ -4,6 +4,7 @@ import Home from "./routes/Home";
 import NotFound from "./routes/NotFound";
 import RoomDetail from "./routes/RoomDetail";
 import GithubConfirm from "./routes/GithubConfirm";
+import KakaoConfirm from "./routes/KakaoConfirm";
 
 const router = createBrowserRouter([
   {
@@ -13,7 +14,13 @@ const router = createBrowserRouter([
     children: [
       { path: "", element: <Home /> },
       { path: "rooms/:roomPk", element: <RoomDetail /> },
-      { path: "social", children: [{ path: "github", element: <GithubConfirm /> }] },
+      {
+        path: "social",
+        children: [
+          { path: "github", element: <GithubConfirm /> },
+          { path: "kakao", element: <KakaoConfirm /> },
+        ],
+      },
     ],
   },
 ]);
